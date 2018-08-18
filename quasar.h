115b10b3186805ca82e73d83f32356f36e3b7089
Tushar Gohad <tushar.gohad@intel.com>
2018-08-18 01:46:29 -0400

    More indent fixes + print exit stats

--- quasar.h
+++ quasar.h
@@ -12,10 +12,10 @@
 #include <strings.h>
 #include <sys/syscall.h>
 
-#define ECHUNK	100		// chunk of events epoll handles at a time
-#define WEBHDR	512		// HTTP response header buffer size
-#define BHSIZE	998888999	// blackhole size
-#define PERIOD	1		// logically report every second
+#define ECHUNK	100             // chunk of events epoll handles at a time
+#define WEBHDR	512             // HTTP response header buffer size
+#define BHSIZE	998888999       // blackhole size
+#define PERIOD	1               // logically report every second
 #define MAXVER	1000
 
 /* modes */
@@ -23,22 +23,22 @@
 #define MODE_READ	1
 
 struct qstat {
-    int id;			// thread id
-    pthread_t tid;		// pthread instance
-    int efd;			// epoll file descriptor
-    int cox;			// connections closed
-    uint64_t twoh;		// 200 OKs;
+    int id;                     // thread id
+    pthread_t tid;              // pthread instance
+    int efd;                    // epoll file descriptor
+    int cox;                    // connections closed
+    uint64_t twoh;              // 200 OKs;
     uint64_t rps;
     uint64_t bps;
     pthread_mutex_t mx;
+    uint64_t curver;            // last count
     uint64_t ver[MAXVER];
-} *th;				// thread specific data
-int scf;			// server-side connection closure flag
-int sre;			// last send_req() error
-int coe;			// new connection error
-int hl, gl;			// HOST and GET sizes
-char *hp, *gp;			// HOST and GET pointers
-uint64_t vernum, vercur;	// version control of HTTP GET
-int running;			// global running flag
+} *th;                          // thread specific data
+int scf;                        // server-side connection closure flag
+int sre;                        // last send_req() error
+int coe;                        // new connection error
+int hl, gl;                     // HOST and GET sizes
+char *hp, *gp;                  // HOST and GET pointers
+int running;                    // global running flag
 
 void *wkr(void *), send_req(int, struct qstat *);
--- send_req.c
+++ send_req.c
@@ -3,58 +3,53 @@
 void send_req(int fd, struct qstat *tt)
 {
     char ver[64];
-    struct iovec iov[6] = { 
-	{.iov_base = "GET ",.iov_len = 4},
-	{.iov_base = gp,.iov_len = gl},
-	{.iov_base = ver,.iov_len = 0},
-	{.iov_base = " HTTP/1.1\r\nHost: ",.iov_len = 17},
-	{.iov_base = hp,.iov_len = hl},
-	{.iov_base = 
-		"\r\nUser-Agent: Quasar\r\nConnection: keep-alive\r\n\r\n",
-		.iov_len = 48}
+    struct iovec iov[6] = {
+        {.iov_base = "GET ",.iov_len = 4},
+        {.iov_base = gp,.iov_len = gl},
+        {.iov_base = ver,.iov_len = 0},
+        {.iov_base = " HTTP/1.1\r\nHost: ",.iov_len = 17},
+        {.iov_base = hp,.iov_len = hl},
+        {.iov_base =
+         "\r\nUser-Agent: Quasar\r\nConnection: keep-alive\r\n\r\n",
+         .iov_len = 48}
     };
     struct msghdr mh = {.msg_name = NULL,
-	.msg_namelen = 0,
-	.msg_iov = iov,
-	.msg_iovlen = 6,	//?sizeof(iov),
-	.msg_control = NULL,
-	.msg_controllen = 0,
-	.msg_flags = 0
+        .msg_namelen = 0,
+        .msg_iov = iov,
+        .msg_iovlen = 6,        //?sizeof(iov),
+        .msg_control = NULL,
+        .msg_controllen = 0,
+        .msg_flags = 0
     };
     struct linger xx = {.l_onoff = 1,.l_linger = 0 };
-    volatile uint64_t a;
 
-    a = vercur;
-    if (++a >= MAXVER) {
-	a = 0;
-    }
-    vercur = a;
+    int a = (int) tt->curver++ % MAXVER;
 
     // format /test/t0009_00522.html
     snprintf(ver, sizeof(ver), "%04llu_%05llu.html",
-	     (unsigned long long int) tt->id,
-	     (unsigned long long int) tt->ver[a]);
+             (unsigned long long int) tt->id,
+             (unsigned long long int) tt->ver[a]);
     if ((gp[gl - 1] == '?') || (gp[gl - 1] == '&') || (gp[gl - 1] == '_')) {
-	iov[2].iov_len = strlen(ver);
+        iov[2].iov_len = strlen(ver);
     } else {
-	iov[2].iov_len = 0;
+        iov[2].iov_len = 0;
     }
     while (((a = sendmsg(fd, &mh, MSG_DONTWAIT | MSG_NOSIGNAL)) < 0)
-	   && (errno == EINTR)) {
-	;
+           && (errno == EINTR)) {
+        ;
     }
     if (a < 0) {
-	sre = errno;
-	setsockopt(fd, SOL_SOCKET, SO_LINGER, &xx, sizeof(xx));
-	while ((close(fd) < 0) && (errno == EINTR)) {
-	    ;
-	}
-	pthread_mutex_lock(&tt->mx);
-	tt->cox++;
-	pthread_mutex_unlock(&tt->mx);
+        sre = errno;
+        setsockopt(fd, SOL_SOCKET, SO_LINGER, &xx, sizeof(xx));
+        while ((close(fd) < 0) && (errno == EINTR)) {
+            ;
+        }
+        pthread_mutex_lock(&tt->mx);
+        tt->cox++;
+        pthread_mutex_unlock(&tt->mx);
     } else {
-	pthread_mutex_lock(&tt->mx);
-	tt->rps++;
-	pthread_mutex_unlock(&tt->mx);
+        pthread_mutex_lock(&tt->mx);
+        tt->rps++;
+        pthread_mutex_unlock(&tt->mx);
     }
 }
